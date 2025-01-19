7503de9e125dd3554ea941327c50ee9e793111f6
junsobi <junsobi@gmail.com>
2025-01-19 22:54:01 +0900

    chore: DrwingState 타입을 types.ts로 이동

--- src/store/drawing-store.ts
+++ src/store/drawing-store.ts
@@ -1,11 +1,5 @@
 import { create } from 'zustand';
-
-type Tool = 'free-draw' | 'line' | 'rect' | 'ellipse' | 'polygon';
-
-interface DrawingState {
-  tool: Tool;
-  setTool: (tool: Tool) => void;
-}
+import { DrawingState } from '@/types/types';
 
 export const useDrawingStore = create<DrawingState>((set) => ({
   tool: 'free-draw',
