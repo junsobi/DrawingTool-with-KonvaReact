7324bafdc97aa9ec47fbac054a748c7974507b91
junsobi <junsobi@gmail.com>
2025-01-20 13:57:49 +0900

    chore: 메타데이터 및 파비콘 업데이트, public 파일 삭제

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -5,7 +5,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex flex-col h-screen bg-slate-50">
       <header className="p-4 flex items-center gap-2 bg-slate-100 shadow-md border-b text-lg font-semibold">
         <PenTool className="size-6" />
-        Drawing Tool
+        Drawing App
       </header>
       <main className="flex-grow flex justify-center items-center">
         {children}
