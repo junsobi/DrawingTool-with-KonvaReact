6bd5a0c205675357d0b1e5ef2bbb6e8b7ab48201
junsobi <junsobi@gmail.com>
2025-01-20 13:18:27 +0900

    refactor: render-shapes 유틸 생성 및 객체 매핑 방식 적용
    - 기존 스위치문을 제거하고 팩토리 패턴을 적용한 객체 매핑 방식으로 변경
    - render-shapes 유틸 파일 추가하여 기존 및 현재 쉐입을 렌더링하는 함수 분리

--- src/components/canvas-component.tsx
+++ src/components/canvas-component.tsx
@@ -2,85 +2,34 @@ import { Stage, Layer } from 'react-konva';
 
 import { useDrawing } from '@/hooks/use-drawing';
 import { useDrawingStore } from '@/store/drawing-store';
-
-import DrawEllipse from './tools/draw-ellipse';
-import DrawLine from './tools/draw-line';
-import DrawRect from './tools/draw-rect';
+import { renderShape, renderTempShape } from '@/utils/render-shapes';
 
 const CanvasComponent = () => {
   const { shapes, color, thickness, tool } = useDrawingStore();
-
   const {
     handleMouseDown,
     handleMouseMove,
     handleMouseUp,
     currentShape,
     isDrawing,
+    polygonPoints,
   } = useDrawing();
 
-  return (
-    <Stage
-      width={800}
-      height={600}
-      className="border rounded-md shadow-sm bg-white"
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-    >
-      <Layer>
-        {shapes.map((shape) => {
-          switch (shape.type) {
-            case 'free-draw':
-            case 'line':
-              return <DrawLine key={shape.id} shape={shape} />;
-            case 'ellipse':
-              return <DrawEllipse key={shape.id} shape={shape} />;
-            case 'rect':
-              return <DrawRect key={shape.id} shape={shape} />;
-            default:
-              return null;
-          }
-        })}
+  const stageProps = {
+    width: 800,
+    height: 600,
+    className: 'border rounded-md shadow-sm bg-white',
+    onMouseDown: handleMouseDown,
+    onMouseMove: handleMouseMove,
+    onMouseUp: handleMouseUp,
+  };
 
-        {isDrawing && (tool === 'free-draw' || tool === 'line') && (
-          <DrawLine
-            shape={{
-              id: 'temp',
-              type: tool,
-              points: currentShape,
-              color,
-              thickness,
-            }}
-          />
-        )}
-        {isDrawing && tool === 'ellipse' && (
-          <DrawEllipse
-            shape={{
-              id: 'temp',
-              type: 'ellipse',
-              x: currentShape[0],
-              y: currentShape[1],
-              radiusX: Math.abs(currentShape[2]),
-              radiusY: Math.abs(currentShape[3]),
-              color,
-              thickness,
-            }}
-          />
-        )}
-        {isDrawing && tool === 'rect' && (
-          <DrawRect
-            shape={{
-              id: 'temp',
-              type: 'rect',
-              x: currentShape[0],
-              y: currentShape[1],
-              width: currentShape[2],
-              height: currentShape[3],
-              color,
-              thickness,
-            }}
-          />
-        )}
+  return (
+    <Stage {...stageProps}>
+      <Layer>
+        {shapes.map(renderShape)}
+        {isDrawing &&
+          renderTempShape(tool, currentShape, color, thickness, polygonPoints)}
       </Layer>
     </Stage>
   );
---
+++ src/utils/render-shapes.tsx
@@ -0,0 +1,81 @@
+import DrawEllipse from '@/components/tools/draw-ellipse';
+import DrawLine from '@/components/tools/draw-line';
+import DrawPolygon from '@/components/tools/draw-polygon';
+import DrawRect from '@/components/tools/draw-rect';
+import { ToolType, Shape } from '@/types/types';
+
+const components: Record<Shape['type'], React.ElementType> = {
+  'free-draw': DrawLine,
+  line: DrawLine,
+  ellipse: DrawEllipse,
+  rect: DrawRect,
+  polygon: DrawPolygon,
+};
+
+export const renderShape = (shape: Shape) => {
+  const Component = components[shape.type];
+  return Component ? <Component key={shape.id} shape={shape} /> : null;
+};
+
+const shapePropsFactory = (
+  tool: ToolType,
+  currentShape: number[],
+  color: string,
+  thickness: number,
+  polygonPoints: number[][]
+) => ({
+  'free-draw': {
+    id: 'temp',
+    color,
+    thickness,
+    type: tool,
+    points: currentShape,
+  },
+  line: { id: 'temp', color, thickness, type: tool, points: currentShape },
+  ellipse: {
+    id: 'temp',
+    color,
+    thickness,
+    type: 'ellipse' as const,
+    x: currentShape[0],
+    y: currentShape[1],
+    radiusX: Math.abs(currentShape[2]),
+    radiusY: Math.abs(currentShape[3]),
+  },
+  rect: {
+    id: 'temp',
+    color,
+    thickness,
+    type: 'rect' as const,
+    x: currentShape[0],
+    y: currentShape[1],
+    width: currentShape[2],
+    height: currentShape[3],
+  },
+  polygon: {
+    id: 'temp',
+    color,
+    thickness,
+    type: 'polygon' as const,
+    points: [...polygonPoints, [currentShape[2], currentShape[3]]],
+    closed: false,
+  },
+});
+
+export const renderTempShape = (
+  tool: ToolType,
+  currentShape: number[],
+  color: string,
+  thickness: number,
+  polygonPoints: number[][]
+) => {
+  const props = shapePropsFactory(
+    tool,
+    currentShape,
+    color,
+    thickness,
+    polygonPoints
+  )[tool];
+  const Component = components[tool];
+  return Component ? <Component shape={props} /> : null;
+};
